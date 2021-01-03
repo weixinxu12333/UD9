41202de9f78eec2bd679c135364569c5389cdd77
Weixin <weixin.xu.2018@gmail.com>
2021-01-03 22:21:05 +0100

    EX2 ACABADO

---
+++ UF9/UF9/EX2/Empleado.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UF9
+{
+    class Empleado
+    {
+        public string nombre { get; set; }
+        public double sueldo { get; set; }
+
+        public Empleado()
+        {
+        }
+
+        public Empleado(string nombre, double sueldo)
+        {
+            this.nombre = nombre;
+            this.sueldo = sueldo;
+        }
+
+        public void ImprimirDatos()
+        {
+            Console.WriteLine("{0} con sueldo mensual {1} euros", nombre, sueldo);
+        }
+
+        public void PagarImpuesto()
+        {
+            if (sueldo >= 3000)
+                Console.WriteLine("{0} tiene que pagar impuesto", nombre);
+            else
+                Console.WriteLine("{0} no tiene que pagar impuesto", nombre);
+        }
+    }
+}
--- UF9/UF9/Program.cs
+++ UF9/UF9/Program.cs
@@ -6,9 +6,19 @@ namespace UF9
     {
         static void Main(string[] args)
         {
-            Alumno alumno = new Alumno("Weixin", 24);
-            alumno.Imprimir();
-            alumno.MayorDeEdad();
+            //Alumno alumno = new Alumno("Weixin", 24);
+            //alumno.Imprimir();
+            //alumno.MayorDeEdad();
+
+            Empleado empleado = new Empleado("A", 1000.50);
+            empleado.ImprimirDatos();
+            empleado.PagarImpuesto();
+
+            Empleado empleado2 = new Empleado();
+            empleado2.nombre = "B";
+            empleado2.sueldo = 3000;
+            empleado2.ImprimirDatos();
+            empleado2.PagarImpuesto();
 
         }
     }
