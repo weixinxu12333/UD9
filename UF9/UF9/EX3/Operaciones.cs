b62292e3e2a83a1742e79bc055bdfef7d67b18f1
Weixin <weixin.xu.2018@gmail.com>
2021-01-03 22:39:34 +0100

    EX3 ACABADO

---
+++ UF9/UF9/EX3/Operaciones.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UF9
+{
+    class Operaciones
+    {
+        int val1 { get; set; }
+        int val2 { get; set; }
+
+        public Operaciones()
+        {
+        }
+
+        public Operaciones(int val1, int val2)
+        {
+            this.val1 = val1;
+            this.val2 = val2;
+        }
+
+        public void suma()
+        {
+            int res = val1 + val2;
+            Console.WriteLine("suma = " + res);
+        }
+
+        public void resta()
+        {
+            int res = val1 - val2;
+            Console.WriteLine("resta = " + res);
+        }
+
+        public void multiplicar()
+        {
+            int res = val1 * val2;
+            Console.WriteLine("multiplicar = " + res);
+        }
+
+        public void dividir()
+        {
+            int res = val1 / val2;
+            Console.WriteLine("dividir = " + res);
+        }
+    }
+}
--- UF9/UF9/Program.cs
+++ UF9/UF9/Program.cs
@@ -10,15 +10,21 @@ namespace UF9
             //alumno.Imprimir();
             //alumno.MayorDeEdad();
 
-            Empleado empleado = new Empleado("A", 1000.50);
-            empleado.ImprimirDatos();
-            empleado.PagarImpuesto();
+            //Empleado empleado = new Empleado("A", 1000.50);
+            //empleado.ImprimirDatos();
+            //empleado.PagarImpuesto();
 
-            Empleado empleado2 = new Empleado();
-            empleado2.nombre = "B";
-            empleado2.sueldo = 3000;
-            empleado2.ImprimirDatos();
-            empleado2.PagarImpuesto();
+            //Empleado empleado2 = new Empleado();
+            //empleado2.nombre = "B";
+            //empleado2.sueldo = 3000;
+            //empleado2.ImprimirDatos();
+            //empleado2.PagarImpuesto();
+
+            Operaciones op = new Operaciones(10,5);
+            op.suma();
+            op.resta();
+            op.multiplicar();
+            op.dividir();
 
         }
     }
