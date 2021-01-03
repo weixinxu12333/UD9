64b70daf60e8da2d1dfb362c9b53a072a091f3d6
Weixin <weixin.xu.2018@gmail.com>
2021-01-03 22:09:27 +0100

    EX1 ACABADO

---
+++ UF9/UF9/EX1/Alumno.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UF9
+{
+    class Alumno
+    {
+        /*
+         Plantear una clase llamada Alumno y definir como atributos su nombre y su edad.
+En el constructor realizar la carga de datos. Definir otros dos métodos para
+imprimir los datos ingresados y un mensaje si es mayor o no de edad (edad >=18)
+         */
+        string nombre {get; set; }
+        int edad {get; set; }
+
+        public Alumno(string nombre, int edad)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+        }
+
+        public void Imprimir()
+        {
+            Console.WriteLine("{0} tiene {1} años", nombre, edad);
+        }
+
+        public void MayorDeEdad()
+        {
+            if (edad >= 18)
+                Console.WriteLine("{0} es mayor de edad", nombre, edad);
+            else
+                Console.WriteLine("{0} no es mayor de edad", nombre, edad);
+        }
+    }
+}
---
+++ UF9/UF9/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UF9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno alumno = new Alumno("Weixin", 24);
+            alumno.Imprimir();
+            alumno.MayorDeEdad();
+
+        }
+    }
+}
